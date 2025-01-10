2f6cf18cfd78a0619c4f47e8f3f826ae4a398ff2
Huang <huang@HuangdeMacBook-Air.local>
2025-01-10 19:09:19 +0900

    add multi-language support: English, (Traditional) Chinese

--- rewrite_with_gpt.py
+++ rewrite_with_gpt.py
@@ -10,12 +10,13 @@ openai.api_key = os.environ.get("OPENAI_API_KEY")
 openai.organization = os.environ.get("OPENAI_ORGANIZATION")
 
 
-def generate_task_description(style_1, style_2, allow_content_change=True):
+def generate_task_description(style_1, style_2,
+                              allow_content_change=True, lang='Japanese'):
     task_descr = [
         "Different people may prefer different types of expression in an advertisement.",
         "Consequently, even for the same content of an advertisement, how it is conveyed may affect the effectiveness of the advertisement.",
         "The following is a tourism advertisement article.",
-        f"Without changing the content and the order of the text, please rewrite the article line by line in Japanese with {style_1} and {style_2} style of speaking, respectively."
+        f"Without changing the content and the order of the text, please rewrite the article line by line in {lang} with {style_1} and {style_2} style of speaking, respectively."
         ]
 
     if not allow_content_change:
@@ -27,10 +28,10 @@ def generate_task_description(style_1, style_2, allow_content_change=True):
     return ' '.join(task_descr)
 
 
-def rewrite(config, sentences):
+def rewrite(config, sentences, args):
     task_descr = generate_task_description(style_1=config['keywords'][0], # ex: 'polite/formal (フォーマル)'
                                            style_2=config['keywords'][1], # ex: 'casual (カジュアル)' 
-                                           allow_content_change=config['allow_content_change'])
+                                           allow_content_change=config['allow_content_change'], lang=args.language)
 
     few_shot_samples = config['few_shots']
 
@@ -78,21 +79,22 @@ def rewrite(config, sentences):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input_fn', default='./data/shimogamo.txt')
-    parser.add_argument('--output_fn', default='./data/shimogamo_4_styles.json')
+    parser.add_argument('-i', '--input_fn', default='./data/shimogamo.txt')
+    parser.add_argument('-o', '--output_fn', default='./data/shimogamo_4_styles.json')
+    parser.add_argument('-l', '--language', default='Japanese')
     args = parser.parse_args()
 
     # extract PR text from file.
     sentences_raw = [sent.strip() for sent in open(args.input_fn, 'r')]
-
     results = {'Raw': sentences_raw}
+
     # step-1: rationality rewrite. 
-    res = rewrite(rationality_config, sentences_raw)
+    res = rewrite(rationality_config, sentences_raw, args)
     for key1 in res: # 'Rational Style' and 'Emotional Style'.
         results.update({key1.split(' ')[0]: res[key1]})
         
         # step-2: politeness rewrite.
-        res_politeness = rewrite(politeness_config, res[key1])
+        res_politeness = rewrite(politeness_config, res[key1], args)
         for key2 in res_politeness: # 'Formal Style' and 'Casual Style':
             results.update({f"{key1.split(' ')[0]}_{key2.split(' ')[0]}": res_politeness[key2]})
 
